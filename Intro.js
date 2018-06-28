109e7f686953ff046c11b9d5ca68fec75eb0cdec
sodapop394 <40676193+sodapop394@users.noreply.github.com>
2018-06-28 15:32:11 -0700

    Add files via upload

---
+++ Intro.js
@@ -0,0 +1,20 @@
+function hello() {
+  document.getElementById("anID").innerHTML="hi"
+}
+
+function time() {
+  document.getElementById("anID2").innerHTML=Date()
+}
+
+function font() {
+  document.getElementById("anID3").style.fontSize='70px'
+}
+function goodbye() {
+  document.getElementById("anID").innerHTML="goodbye"
+}
+function resize() {
+  document.getElementById("anID3").style.fontSize='20px'
+}
+function picchange() {
+  document.getElementById("picture").src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ33rmMRQNmPqlmLx1dzK5O7sW4yAxD_U5vm4wf-VitkhkmMi8yQA'
+}
